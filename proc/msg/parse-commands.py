af49b093f17cfb11bcec832dcf0aa7ecd1eefe44
Marcel Lauhoff <ml@irq0.org>
2014-02-04 02:12:56 +0100

    Add tool to parse irc messages for commands
    Proof of concept: Parse !tts and enqueue messages in tts queue

---
+++ proc/msg/parse-commands.py
@@ -0,0 +1,41 @@
+import pika
+import sys
+import time
+import json
+import re
+
+conn = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+chan = conn.channel()
+
+result = chan.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+chan.exchange_declare(exchange="msg",
+                      type="topic")
+
+chan.queue_bind(exchange="msg",
+                queue=queue_name,
+                routing_key="#.recv.raw")
+
+print "Using queue:", queue_name
+print "Waiting for messages.."
+
+
+def callback(ch, method, props, body):
+    print " [x] recvd %r %r" % (method.routing_key, body)
+
+    if props.content_type == "application/json":
+        msg = json.loads(body)
+
+        if re.match(r"^!tts", msg["msg"]):
+            chan.basic_publish(exchange="",
+                               routing_key="tts",
+                               body=msg["msg"][len("!tts "):])
+
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+chan.basic_consume(callback,
+                   queue=queue_name)
+
+chan.start_consuming()
+conn.close()
