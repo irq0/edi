37f92dfbe7b3430b92d8ad86d4a056bb209a045e
Marcel Lauhoff <ml@irq0.org>
2014-02-04 15:56:31 +0100

    parse-commands: "!(\w+)" -> \1 = command

--- proc/msg/parse-commands.py
+++ proc/msg/parse-commands.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+
+#
+# Parse commands from messages (think of irc, jabber,..).
+# The idea is to loosly parse things that look like commands and add them to
+# the cmd exchange with a routing key resembling that command
+#
+
+
+# TODO verify users somehow
+# TODO add user authorizations?
+
 import pika
 import sys
 import time
@@ -17,24 +29,41 @@ chan.queue_bind(exchange="msg",
                 queue=queue_name,
                 routing_key="#.recv.raw")
 
-print "Using queue:", queue_name
-print "Waiting for messages.."
+print "---- Using queue:", queue_name
+print "---- Waiting for messages:"
 
+def publish(key, data):
+        print "---> [%r] %r" % (key, data)
+        chan.basic_publish(exchange="cmd",
+                           routing_key=key,
+                           body=data,
+                           properties=pika.BasicProperties(
+                               content_type="application/json",
+                               delivery_mode=2))
 
-def callback(ch, method, props, body):
-    print " [x] recvd %r %r" % (method.routing_key, body)
+def recv(ch, method, props, body):
+    print "<--- [%r] %r" % (method.routing_key, body)
 
     if props.content_type == "application/json":
         msg = json.loads(body)
 
-        if re.match(r"^!tts", msg["msg"]):
-            chan.basic_publish(exchange="tts_worker",
-                               routing_key="tts",
-                               body=msg["msg"][len("!tts "):])
+        m = re.search(r"^!(\w+)\s?(.*?)$", msg["msg"])
 
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+        if m:
+            assert len(m.groups()) == 2
 
-chan.basic_consume(callback,
+            cmd, args = m.groups()
+            publish(cmd,
+                    json.dumps({
+                        "cmd" : cmd,
+                        "args" : args.split(),
+                        "user" : msg["user"],
+                        "src" : method.routing_key,}))
+
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+
+
+chan.basic_consume(recv,
                    queue=queue_name)
 
 chan.start_consuming()
