f085f0de8a59a69f4b05d67cb47cbba6b099b055
Marcel Lauhoff <ml@irq0.org>
2014-02-14 16:04:23 +0100

    Refactor handler
    * Move state change functions to state
    * Move formatters to util

--- src/thehonestbookoftruth/handler.clj
+++ src/thehonestbookoftruth/handler.clj
@@ -1,8 +1,6 @@
 (ns thehonestbookoftruth.handler
   (:use [clj-time.local :only [local-now]]
-        [clj-time.coerce :only [to-date from-date]]
-        [clj-time.core :as t :only [interval in-minutes]]
-        [thehonestbookoftruth.util :only [parse-eta format-eta format-time-span]])
+        [thehonestbookoftruth.util :only [parse-eta format-eta format-time-span format-user-list]])
   (:require [thehonestbookoftruth.state :as state]
             [taoensso.timbre :as timbre]))
 
@@ -19,59 +17,47 @@
 
 (defmethod handler :help
   [{:keys [args]}]
+
   (when (= args "thehonestbookoftruth")
     help))
 
-(defn- format-ul-user [[user vals]]
-  (let [{:keys [eta ts]} vals]
-    (cond
-      eta (str user " (ETA: " (format-eta (from-date eta))
-            " [" (format-time-span (local-now) (from-date eta)) " min])")
-      ts (str user " (SINCE: " (format-eta (from-date ts))
-           " [" (format-time-span (from-date ts) (local-now)) " min])"))))
-
-
 (defmethod handler :ul [_]
-  (apply str
-    (interpose ", "
-      (map format-ul-user @state/db))))
+  (format-user-list @state/*db*))
 
 (defmethod handler :login
   [{:keys [user]}]
-  (if (:ts (@state/db user))
+
+  (if (state/logged-in? user)
     (str "Already logged in!")
-    (do
-      (swap! state/db (fn [old]
-                        (-> old
-                          (assoc-in [user :ts] (to-date (local-now)))
-                          (assoc-in [user :eta] nil))))
-      (str "Hi, " user))))
+    (and (state/login! user) (str "Hi, " user))))
 
 (defmethod handler :logout
   [{:keys [user]}]
-  (if (:ts (@state/db user))
-    (swap! state/db (fn [old] (dissoc old user)))
+
+  (if (state/logged-in? user)
+    (state/logout! user)
     "Hmm, you are not logged in. So no logout ;)"))
 
 (defmethod handler :eta
   [{:keys [user args]}]
-  (if (:ts (@state/db user))
+
+  (if (state/logged-in? user)
     "Logged in. I'll ignore the ETA :P"
     (try
       (let [eta (parse-eta args)
             span (format-time-span (local-now) eta)]
-        (swap! state/db (fn [old] (assoc old user {:eta (to-date eta)})))
-        (str "Cya in " span " minutes"))
+        (and (state/set-eta! user eta)
+          (str "Cya in " span " minutes")))
       (catch java.lang.IllegalArgumentException e
         (str "Hmpf. \"" args "\" isn't a valid ETA")))))
 
 (defmethod handler :uneta
   [{:keys [user]}]
-  (if (:eta (@state/db user))
-    (do
-      (swap! state/db (fn [old] (assoc-in old [user :eta] nil)))
-      "Schade")
-    "No ETA"))
+
+  (if (state/has-eta? user)
+    (and (state/unset-eta! user)
+      "Schade :(")
+    "No ETA set :P"))
 
 (defmethod handler :default [args]
   (error "Command not supported")
--- src/thehonestbookoftruth/state.clj
+++ src/thehonestbookoftruth/state.clj
@@ -1,5 +1,7 @@
 (ns thehonestbookoftruth.state
-  (:require [clojure.java.io   :as io]
+  (:use [clj-time.coerce :only [to-date from-date]]
+        [clj-time.local :only [local-now]])
+  (:require [clojure.java.io :as io]
             [taoensso.timbre :as timbre]
             [clojurewerkz.serialism.core :as s]))
 
@@ -7,11 +9,11 @@
 
 (def ^:dynamic *db-file* "/tmp/eta.edn")
 
-(def db (atom {}))
+(def ^:dynamic *db* (atom {}))
 
-(defn persist-db! [_ _ _ db]
+(defn persist-db! [_ _ _ data]
   (info "DB saved")
-  (spit *db-file* (s/serialize db s/clojure-content-type)))
+  (spit *db-file* (s/serialize data s/clojure-content-type)))
 
 (defn log-db-changes [_ _ old new]
   (info (str "Old db: " old))
@@ -21,9 +23,30 @@
   (s/deserialize (slurp *db-file*) s/clojure-content-type))
 
 (defn init-from-file! []
-  (reset! db (load-db))
-  (info (str "Initialized db from file: " @db)))
+  (reset! *db* (load-db))
+  (info (str "Initialized db from file: " @*db*)))
 
 (defn init-watches []
-  (add-watch db :persist persist-db!)
-  (add-watch db :log log-db-changes))
+  (add-watch *db* :persist persist-db!)
+  (add-watch *db* :log log-db-changes))
+
+(defn logged-in? [user]
+  (boolean (:ts (@*db* user))))
+
+(defn has-eta? [user]
+  (boolean (:eta (@*db* user))))
+
+(defn logout! [user]
+  (swap! *db* (fn [old] (dissoc old user))))
+
+(defn login! [user]
+  (swap! *db* (fn [old]
+              (-> old
+                (assoc-in [user :ts] (to-date (local-now)))
+                (assoc-in [user :eta] nil)))))
+
+(defn set-eta! [user ^org.joda.time.DateTime eta]
+  (swap! *db* (fn [old] (assoc old user {:eta (to-date eta)}))))
+
+(defn unset-eta! [user]
+  (swap! *db* (fn [old] (assoc-in old [user :eta] nil))))
