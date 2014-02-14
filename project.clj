aa59a99f9f3ed782eb8824a15f791cdd914dfe03
Marcel Lauhoff <ml@irq0.org>
2014-02-14 16:31:49 +0100

    project.clj Add descr, url, version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject thehonestbookoftruth "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject thehonestbookoftruth "0.1.0"
+  :description "EDI MQ User presence, eta, etc. "
+  :url "https://git.c3pb.de/irq0/thehonestbookoftruth"
   :main thehonestbookoftruth.core
   :aot [thehonestbookoftruth.core]
   :dependencies [[org.clojure/clojure "1.5.1"]
