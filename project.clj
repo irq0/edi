bb03400916c6118e2d72e7d0f7629a4d02542a8f
Marcel Lauhoff <ml@irq0.org>
2014-02-14 02:52:07 +0100

    Add lein project file

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject thehonestbookoftruth "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :main thehonestbookoftruth.core
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.novemberain/langohr "2.3.2"]
+                 [clj-time "0.6.0"]
+                 [com.taoensso/timbre "3.0.1"]
+                 [clojurewerkz/serialism "1.0.1"]])
