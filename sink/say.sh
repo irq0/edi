56c75f8ec143ca909eb5045ca0186b1305b441eb
Marcel Lauhoff <ml@irq0.org>
2014-02-14 16:39:06 +0100

    Add sink/say.sh

--- sink/say.sh
+++ sink/say.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-amqp-consume --url="amqp://localhost" --exchange="notify" --routing-key="audio" mplayer -
-#amqp-consume --url="amqp://localhost" --exchange="notify" --routing-key="audio" oggdec -
+amqp-consume --url="amqp://localhost" --exchange=say --routing-key=""  mplayer -
