d6a12e7d5fa97824a9181c6695f26ae47714c34d
Marcel Lauhoff <ml@irq0.org>
2014-02-18 02:37:47 +0100

    Squashed commit of the following:
    commit 868f86c80470e7baa430a803a2befff705931af2
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Tue Feb 18 01:50:39 2014 +0100
    
        Reploy JSON messages
    
    commit 5acddbdf0e482d74037dfbbbc9af21479ebdb6b7
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Mon Feb 17 20:12:52 2014 +0100
    
        Add logout-all command
    
    commit aa59a99f9f3ed782eb8824a15f791cdd914dfe03
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Fri Feb 14 16:31:49 2014 +0100
    
        project.clj Add descr, url, version
    
    commit a36a97969af56035392462d8deb121e7dbabdaaf
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Fri Feb 14 16:30:13 2014 +0100
    
        Add lein gitignore
    
    commit 6abf4eb23464007811f6c62b34b2f83668ca7fa5
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Fri Feb 14 16:29:55 2014 +0100
    
        Make uberjar ready
    
    commit 99ca07404deae476723ac7cbd94834b0c71df4cb
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Fri Feb 14 16:26:56 2014 +0100
    
        Improve logging
    
    commit a37e3c7cf4c0701c0367ddb207c7cf40745b0f30
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Fri Feb 14 16:08:44 2014 +0100
    
        handler: Add logout message
    
    commit e5ab2c151c1c983741aeeb80345f94fd288424e0
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Fri Feb 14 16:05:01 2014 +0100
    
        Add tests for state and util
    
    commit f085f0de8a59a69f4b05d67cb47cbba6b099b055
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Fri Feb 14 16:04:23 2014 +0100
    
        Refactor handler
    
        * Move state change functions to state
        * Move formatters to util
    
    commit bb03400916c6118e2d72e7d0f7629a4d02542a8f
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Fri Feb 14 02:52:07 2014 +0100
    
        Add lein project file
    
    commit c7b23864444acd9ab45d7a771ebcd38d3cfa90e5
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Fri Feb 14 02:51:56 2014 +0100
    
        Add README
    
    commit d27cf3624f458efd9c0820c197efc00b3b7d6510
    Author: Marcel Lauhoff <ml@irq0.org>
    Date:   Fri Feb 14 02:51:28 2014 +0100
    
        User presence and ETA

---
+++ proc/thehonestbookoftruth/src/thehonestbookoftruth/core.clj
@@ -0,0 +1,57 @@
+(ns thehonestbookoftruth.core
+  (:use [thehonestbookoftruth.handler :only [handler]])
+  (:require [langohr.core      :as rmq]
+            [langohr.channel   :as lch]
+            [langohr.queue     :as lq]
+            [langohr.consumers :as lc]
+            [langohr.basic     :as lb]
+            [clojure.string    :as str]
+            [taoensso.timbre :as timbre]
+            [clojurewerkz.serialism.core :as s]
+            [thehonestbookoftruth.state :as state])
+  (:gen-class))
+
+(timbre/refer-timbre)
+
+(defn reply [ch orig reply]
+  (let [dst (str/replace (:src orig) #"recv" "send")
+        msg (s/serialize {:user (:user orig) :msg reply} :json)]
+    (info (format "---> Handler reply orig_msg=%s key=%s: %s" orig dst msg))
+    (lb/publish ch "msg" dst msg :content-type "application/json")))
+
+(defn message-handler [ch {:keys [content-type delivery-tag] :as meta} ^bytes payload]
+  (info (format "<--- Received message: %s, content type: %s"
+          (String. payload "UTF-8") content-type))
+
+  (when (= content-type "application/json")
+    (try
+      (let [msg (s/deserialize payload :json)]
+        (reply ch msg (handler msg)))
+      (catch com.fasterxml.jackson.core.JsonParseException e
+        (error "[HANDLER] json decode failed :("))))
+  (lb/ack ch delivery-tag))
+
+
+(defn -main [& args]
+  (let [url   (or (first args) (System/getenv "AMQP_URL") "amqp://localhost")
+        conn  (rmq/connect {:uri url})
+        ch    (lch/open conn)
+        keys  ["login" "logout" "logout-all" "ul" "eta" "uneta" "help" "list"]
+        ex    "cmd"
+        qname (:queue (lq/declare ch))]
+
+
+    (state/init-from-file!)
+    (state/init-watches)
+
+    (info (format "[MAIN] Connected. Channel id: %d" (.getChannelNumber ch)))
+
+    (doseq [k keys]
+      (lq/bind ch qname ex :routing-key k))
+
+    (lc/blocking-subscribe ch qname message-handler)
+
+    (info "[MAIN] Disconnecting...")
+
+    (rmq/close ch)
+    (rmq/close conn)))
