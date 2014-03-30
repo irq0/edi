dca962b245d83d289cefeac055cee50e41def8dd
Marcel Lauhoff <ml@irq0.org>
2014-03-30 15:28:49 +0200

    mqbot: Add IRC NAMES support

--- bot/irc/mqbot.py
+++ bot/irc/mqbot.py
@@ -9,7 +9,7 @@
 
 
 from twisted.words.protocols import irc
-from twisted.internet import reactor, protocol, ssl
+from twisted.internet import reactor, protocol, ssl, defer
 from twisted.python import log
 
 from threading import Thread
@@ -186,7 +186,44 @@ class MQ(Thread):
         except IOError, e:
             print "Error while closing amqp connection:", e
 
-class MQBot(irc.IRCClient):
+class NamesIRCClient(irc.IRCClient):
+    def __init__(self, *args, **kwargs):
+        self._namescallback = {}
+
+    def names(self, channel):
+        channel = channel.lower()
+        d = defer.Deferred()
+        if channel not in self._namescallback:
+            self._namescallback[channel] = ([], [])
+
+        self._namescallback[channel][0].append(d)
+        self.sendLine("NAMES %s" % channel)
+        return d
+
+    def irc_RPL_NAMREPLY(self, prefix, params):
+        channel = params[2].lower()
+        nicklist = params[3].split(' ')
+
+        if channel not in self._namescallback:
+            return
+
+        n = self._namescallback[channel][1]
+        n += nicklist
+
+    def irc_RPL_ENDOFNAMES(self, prefix, params):
+        channel = params[1].lower()
+        if channel not in self._namescallback:
+            return
+
+        callbacks, namelist = self._namescallback[channel]
+
+        for cb in callbacks:
+            cb.callback(namelist)
+
+        del self._namescallback[channel]
+
+
+class MQBot(NamesIRCClient):
     """http://twistedmatrix.com/documents/8.2.0/api/twisted.words.protocols.irc.IRCClient.html"""
 
     nickname = "EDI"
