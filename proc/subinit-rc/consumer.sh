17e17c8f33a9b66875123123447595a62ef20ac9
edi <edi@irq0.org>
2014-04-15 21:04:49 +0200

    subinit-rc: Support AMQP_SERVER env in consumer.sh

--- proc/subinit-rc/consumer.sh
+++ proc/subinit-rc/consumer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec amqp-consume -u 'amqp://localhost' -e subinit -q "subinit-rc-$(hostname)" -r 'rc.#' $(dirname $0)/bin/run-rc.sh
+exec amqp-consume -u "amqp://${AMQP_SERVER:-localhost}" -e subinit -q "subinit-rc-$(hostname)" -r 'rc.#' $(dirname $0)/bin/run-rc.sh
