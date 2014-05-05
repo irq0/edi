f48934935c428b34a6118e1ca13f677d7e2d6cc7
Marcel Lauhoff <ml@irq0.org>
2014-05-05 02:41:39 +0200

    parse-commands: Use pyedi

--- proc/msg/parse-commands.py
+++ proc/msg/parse-commands.py
@@ -1,71 +1,46 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
 
 #
 # Parse commands from messages (think of irc, jabber,..).
-# The idea is to loosly parse things that look like commands and add them to
-# the cmd exchange with a routing key resembling that command
 #
 
-
-# TODO verify users somehow
-# TODO add user authorizations?
-
-import pika
+import edi
 import sys
 import time
-import json
-import re
 
-conn = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-chan = conn.channel()
+import logging
 
-result = chan.queue_declare(exclusive=True)
-queue_name = result.method.queue
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("parse-commands")
 
-chan.exchange_declare(exchange="msg",
-                      durable=True,
-                      type="topic")
+with edi.Manager() as e:
+        @edi.edi_msg(e, "#.recv.*")
+        @edi.edi_filter_msg_with_uflag("op")
+        @edi.edi_filter_msg_matches(r"^!(\w+)\s?(.*?)$")
+        def parse_cmds(regroups, **msg):
+                cmd, args = regroups
 
-chan.queue_bind(exchange="msg",
-                queue=queue_name,
-                routing_key="#.recv.*")
+                try:
+                        user = msg["user"]
+                except KeyError:
+                        user = "NA"
 
-print "---- Using queue:", queue_name
-print "---- Waiting for messages:"
+                edi.emit.cmd(e.chan,
+                             cmd=cmd,
+                             args=args,
+                             user=user,
+                             src=msg["rkey"])
 
-def publish(key, data):
-        print "---> [%r] %r" % (key, data)
-        chan.basic_publish(exchange="cmd",
-                           routing_key=key,
-                           body=data,
-                           properties=pika.BasicProperties(
-                               content_type="application/json",
-                               delivery_mode=2))
-
-def recv(ch, method, props, body):
-    print "<--- [%r] %r" % (method.routing_key, body)
-
-    if props.content_type == "application/json":
-        msg = json.loads(body)
-
-        m = re.search(r"^!(\w+)\s?(.*?)$", msg["msg"])
-
-        if m and "op" in msg["uflags"]:
-            assert len(m.groups()) == 2
-
-            cmd, args = m.groups()
-            publish(cmd,
-                    json.dumps({
-                        "cmd" : cmd,
-                        "args" : args,
-                        "user" : msg["user"],
-                        "src" : method.routing_key,}))
-
-        ch.basic_ack(delivery_tag = method.delivery_tag)
-
-
-chan.basic_consume(recv,
-                   queue=queue_name)
-
-chan.start_consuming()
-conn.close()
+        @edi.edi_msg(e, "#.recv.*")
+        @edi.edi_filter_msg_without_uflag("op")
+        @edi.edi_filter_msg_matches(r"^!(\w+)\s?(.*?)$")
+        def reply_unauthorized(regroups, **msg):
+                if msg.has_key("user"):
+                        edi.emit.msg_reply(e.chan,
+                                           src=msg["rkey"],
+                                           user=msg["user"],
+                                           msg="No OP. No commands :P")
+        e.run()
