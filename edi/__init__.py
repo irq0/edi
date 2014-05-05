48ee6a7ce1803b062ffdf1fc84dcb6e44f0dd28c
Marcel Lauhoff <ml@irq0.org>
2014-05-05 04:50:25 +0200

    Squashed 'lib/python/' changes from fc7f50f..72360f3
    72360f3 Add generic emit, Handle broken reply sources
    afafcd3 Rename edi_msg_filter_matches -> edi_filter_matches
    
    git-subtree-dir: lib/python
    git-subtree-split: 72360f347a77556518b0470f368dede8341eb124

--- edi/__init__.py
+++ edi/__init__.py
@@ -6,4 +6,4 @@ __version__ = "0.1"
 __author__ = "Marcel Lauhoff"
 
 from .core import Manager
-from .decorators import edi_msg, edi_cmd, edi_msg_recv, edi_msg_recv_proto, edi_msg_recv_ircchan, edi_filter_msg_with_uflag, edi_filter_msg_without_uflag, edi_filter_msg_matches
+from .decorators import edi_msg, edi_cmd, edi_msg_recv, edi_msg_recv_proto, edi_msg_recv_ircchan, edi_filter_msg_with_uflag, edi_filter_msg_without_uflag, edi_filter_matches
