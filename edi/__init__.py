fc7f50f2ea8ae7ddd16420875612b97c5eb16054
Marcel Lauhoff <ml@irq0.org>
2014-05-05 02:17:32 +0200

    Add decorators to module import list

--- edi/__init__.py
+++ edi/__init__.py
@@ -5,8 +5,5 @@ __title__ = "edi"
 __version__ = "0.1"
 __author__ = "Marcel Lauhoff"
 
-
 from .core import Manager
-from .decorators import edi_msg, edi_cmd, edi_filter_msg_with_uflag, edi_filter_msg_matches
-
-#foo
+from .decorators import edi_msg, edi_cmd, edi_msg_recv, edi_msg_recv_proto, edi_msg_recv_ircchan, edi_filter_msg_with_uflag, edi_filter_msg_without_uflag, edi_filter_msg_matches
