06644d26a6d10cda8c40e7712898f451185a9446
Marcel Lauhoff <ml@irq0.org>
2014-05-11 17:42:39 +0200

    thehonestbookoftruth: Update dependencies

--- apps/thehonestbookoftruth/project.clj
+++ apps/thehonestbookoftruth/project.clj
@@ -3,8 +3,8 @@
   :url "https://git.c3pb.de/irq0/thehonestbookoftruth"
   :main thehonestbookoftruth.core
   :aot [thehonestbookoftruth.core]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.novemberain/langohr "2.3.2"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.novemberain/langohr "2.9.0"]
                  [clj-time "0.6.0"]
-                 [com.taoensso/timbre "3.0.1"]
-                 [clojurewerkz/serialism "1.0.1"]])
+                 [com.taoensso/timbre "3.2.1"]
+                 [clojurewerkz/serialism "1.2.0"]])
