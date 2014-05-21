5d0c4e396974073ac5563db795aa7d74e0070ea5
Gregor Best <gbe@ring0.de>
2014-05-21 18:37:40 +0200

    Change version to something sane
    Signed-off-by: Gregor Best <gbe@ring0.de>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pizzamaschine "0.1.0-SNAPSHOT"
+(defproject pizzamaschine "1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
