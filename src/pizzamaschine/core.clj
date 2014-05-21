ed106f1778cbe78fae3c97427948883505b92933
Gregor Best <gbe@ring0.de>
2014-05-21 22:06:52 +0200

    Clean up a bit
    Signed-off-by: Gregor Best <gbe@ring0.de>

--- src/pizzamaschine/core.clj
+++ src/pizzamaschine/core.clj
@@ -17,8 +17,7 @@
   (get (System/getenv)
        "CLOUDAMQP_URL" "amqp://guest:guest@mopp"))
 
-(defn now []
-  (java.util.Date.))
+(def +state-path+ "/tmp/pizza.json")
 
 (def +help-message+
   (str
@@ -27,24 +26,58 @@
     "!pizza-help       -- Diese Nachricht\n"
     "!pizza $something -- Bestellt $something"))
 
+(defn now []
+  (java.util.Date.))
+
 (def +orders+ (ref '{}))
 (def +first-order+ (ref ""))
 (def +started-by+ (ref ""))
 
+(defn store-state
+  ([] (store-state +state-path+))
+  ([path]
+   (try
+     (dosync
+       (spit path
+             (str (json/write-str
+                    {"orders" @+orders+
+                     "first-order" @+first-order+
+                     "started-by" @+started-by+})
+                  "\n"))
+       true)
+     (catch Exception e
+       (println (str "Could not store state in " path ":\n"
+                     (.getMessage e)))
+       false))))
+
+(defn load-state!
+  ([] (load-state! +state-path+))
+  ([path]
+   (dosync
+     (try
+       (let [data (json/read-str (slurp path))]
+         (alter +orders+ (fn [x] (get data "orders")))
+         (alter +first-order+ (fn [x] (get data "first-order")))
+         (alter +started-by+ (fn [x] (get data "started-by")))
+         true)
+       (catch Exception e
+         (println (str "Could not load state from " path ":\n"
+                       (.getMessage e)))
+         false)))))
+
 (defn list-orders []
   (dosync
-  (let [f @+first-order+]
-    (if (empty? f)
+    (if (empty? @+orders+)
       "Keine Bestellungen :/"
       (str
         (apply str
                (interpose "\n"
-                           (map #(str (first %) ": " (second %))
-                                @+orders+)))
-        "\n(gestartet: " f " von " @+started-by+ ")\n"
-        )))))
+                          (map #(str (first %) ": " (second %))
+                               @+orders+)))
+        "\n(gestartet: " @+first-order+ " von " @+started-by+ ")\n"
+        ))))
 
-(defn add-order [user order]
+(defn add-order! [user order]
   (dosync
     (when (empty? @+first-order+)
       (alter +first-order+ (fn [x] (str (now))))
@@ -53,26 +86,29 @@
     (str "Ack: "
          order)))
 
+(defn reset-orders! []
+  (dosync
+    (let [o (list-orders)]
+      (alter +orders+ (fn [x] '{}))
+      (str o
+           (when-not (empty? @+first-order+)
+             (alter +first-order+ (fn [x] ""))
+             "\nBestelliste wurde geleert.")))))
+
 (defn dispatch-command [msg]
   (let [args (:args msg)
         user (:user msg)
         cmd  (:cmd msg)]
     (cond
       (= cmd "pizza-reset")
-      (dosync
-        (let [o (list-orders)]
-          (alter +orders+ (fn [x] '{}))
-          (str o
-               (when-not (empty? @+first-order+)
-                 (alter +first-order+ (fn [x] ""))
-                 "\nBestelliste wurde geleert."))))
+      (reset-orders!)
       (= cmd "pizza-list")
       (list-orders)
       (or (= cmd "pizza-help")
           (empty? args))
       +help-message+
       :else
-      (add-order user args))))
+      (add-order! user args))))
 
 (defn message-handler
   [ch {:keys [content-type delivery-tag] :as meta} ^bytes payload]
@@ -90,18 +126,19 @@
 (defn setup
   ([] (setup false))
   ([blocking]
-    (let [conn  (rmq/connect {:uri (make-url)})
-          ch    (lch/open conn)
-          qname (:queue (lq/declare ch))
-          exchange "cmd"
-          keys ["pizza" "pizza-reset" "pizza-list" "pizza-help"]]
-      (doseq [k keys]
-        (println "Binding to" exchange "/" k)
-        (lq/bind ch qname exchange :routing-key k))
-      (if blocking
-        (lc/blocking-subscribe ch qname #'message-handler)
-        (lc/subscribe ch qname #'message-handler))
-      {:conn conn :channel ch})))
+   (load-state!)
+   (let [conn  (rmq/connect {:uri (make-url)})
+         ch    (lch/open conn)
+         qname (:queue (lq/declare ch))
+         exchange "cmd"
+         keys ["pizza" "pizza-reset" "pizza-list" "pizza-help" "pizza-debug"]]
+     (doseq [k keys]
+       (println "Binding to" exchange "/" k)
+       (lq/bind ch qname exchange :routing-key k))
+     (if blocking
+       (lc/blocking-subscribe ch qname #'message-handler)
+       (lc/subscribe ch qname #'message-handler))
+     {:conn conn :channel ch})))
 
 (comment
   (rmq/close (:conn *conn*))
