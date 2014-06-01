def120e8a44b3011b36b6c1dfc513440862dadf3
Marcel Lauhoff <ml@irq0.org>
2014-06-01 17:40:11 +0200

    Revert "core: Add random id to default app name"
    This reverts commit d84f13262d458d1c124bed10e2b620bdaddd5b2f.

--- edi/core.py
+++ edi/core.py
@@ -91,7 +91,7 @@ class Manager(object):
     consumer_tags = []
 
     metadata = {
-        "app" : "unknown-" + binascii.b2a_hex(os.urandom(6)),
+        "app" : "unknown",
         "descr" : "unnamed pyedi app",
         "cmds" : {}
     }
@@ -159,10 +159,8 @@ class Manager(object):
                   cmd, args, descr, attribs)
 
     def _make_queue_name(self, suffix):
-        app = self.metadata["app"]
-
         return "pyedi_{}__{}".format(
-            re.sub(r"[^\w]", "", app),
+            re.sub(r"[^\w]", "", self.metadata["app"]),
             suffix)
 
     def register_inspect_command(self):
