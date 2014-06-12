eff50f965c962c3cd0923c06a724864c172320eb
Marcel Lauhoff <ml@irq0.org>
2014-06-13 01:12:54 +0200

    msg-to-cmd: Two user levels: full and guest. Use whitelist for guest cmds

--- apps/msg-to-cmd/parse-commands.py
+++ apps/msg-to-cmd/parse-commands.py
@@ -3,49 +3,71 @@
 
 from __future__ import unicode_literals
 
-#
-# Parse commands from messages (think of irc, jabber,..).
-#
-
 import edi
-import sys
-import time
-
 import logging
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("parse-commands")
+log = logging.getLogger("msg-to-cmd")
+
+
+# Parse commands from messages (think of irc, jabber,..).
+# Two access levels:
+# full: All commands allowed
+# guest: Only whitelisted commands allowed
 
 CMD_REGEX = r"^!([\w-]+)\s?(.*?)$"
 
-with edi.Manager() as e:
-        @edi.edi_msg(e, "#.recv.*")
-        @edi.edi_filter_msg_with_uflag_any(["op", "voice"])
-        @edi.edi_filter_matches(CMD_REGEX)
-        def parse_cmds(regroups, **msg):
-                cmd, args = regroups
+GUEST_CMD_WHITELIST = (
+    "ul",
+    "pizza-list",
+    "pizza",
+    "pizza-help",
+    "help",
+    "describe",
+    "apps",
+    "commands")
 
-                try:
-                        user = msg["user"]
-                except KeyError:
-                        user = "NA"
+def do_cmd(args, cmd, cmd_args):
+    log.info("[CMD] rkey=%r, user=%r uflags=%r: %s %s",
+             args["rkey"], args["user"], args["uflags"], cmd, cmd_args)
+    edi.emit.cmd(e.chan,
+                 cmd=cmd,
+                 args=cmd_args,
+                 user=args["user"],
+                 src=args["rkey"])
 
-                edi.emit.cmd(e.chan,
-                             cmd=cmd,
-                             args=args,
-                             user=user,
-                             src=msg["rkey"])
+def reply_error(args, err):
+    log.info("[UNAUTHORIZED] rkey=%r, user=%r uflags=%r",
+             args["rkey"], args["user"], args["uflags"])
 
-        @edi.edi_msg(e, "#.recv.*")
-        @edi.edi_filter_msg_with_uflag_none(["op", "voice"])
-        @edi.edi_filter_matches(CMD_REGEX)
-        def reply_unauthorized(regroups, **msg):
-            m="No OP/voice, No commands :P (Hint: https://www.c3pb.de/_media/wiki/documents/mitgliedsantrag.pdf)"
-            if msg.has_key("user"):
-                edi.emit.msg_reply(e.chan,
-                        src=msg["rkey"],
-                        user=msg["user"],
-                        msg=m)
+    edi.emit.msg_reply(e.chan,
+                       src=args["rkey"],
+                       user=args["user"],
+                       msg=err)
 
-        e.register_inspect_command()
-        e.run()
+
+with edi.Manager(descr="Parse special strings from messages as EDI commands",
+                 name="msg-to-cmd") as e:
+    @edi.edi_msg(e, "#.recv.*")
+    @edi.edi_filter_matches(CMD_REGEX)
+    def parse_cmds(regroups, **args):
+        cmd, cmd_args = regroups
+
+        is_full = "op" in args["uflags"]
+        is_guest = "voice" in args["uflags"]
+        cmd_whitelisted = cmd in GUEST_CMD_WHITELIST
+
+        if is_full or (is_guest and cmd_whitelisted):
+            do_cmd(args, cmd, cmd_args)
+
+        elif is_guest and not cmd_whitelisted:
+            m = "Guests are only allowed a subset of commands: \n" + \
+                ",".join(GUEST_CMD_WHITELIST)
+            reply_error(args, m)
+        else:
+            m = "No OP/voice, No commands :P" \
+                "(Hint: https://www.c3pb.de/_media/wiki/documents/mitgliedsantrag.pdf)"
+            reply_error(args, m)
+
+    e.register_inspect_command()
+    e.run()
