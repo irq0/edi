8354b1859a55eee15fa60493755895cb6cb9c162
Gregor Best <gbe@ring0.de>
2014-09-25 15:59:02 +0200

    Pull in EDI code

---
+++ apps/pizzamaschine/test/pizzamaschine/core_test.clj
@@ -0,0 +1,7 @@
+(ns pizzamaschine.core-test
+  (:require [clojure.test :refer :all]
+            [pizzamaschine.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
